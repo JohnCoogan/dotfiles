c0a8ec8ae668aea18cb7ecd4a5d1b384747a54b1
John Coogan <coogan.johna@gmail.com>
2015-03-03 21:15:30 -0800

    added sudo for making directories during install

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
+sudo -v
 
 # paths
 dirname=$(pwd)
@@ -5,8 +6,8 @@ lib="/usr/local/lib"
 bin="/usr/local/bin"
 
 # make in case they aren't already there
-mkdir -p "/usr/local/lib"
-mkdir -p "/usr/local/bin"
+sudo mkdir -p "/usr/local/lib"
+sudo mkdir -p "/usr/local/bin"
 
 # Copy the path
 sudo cp -R $dirname "$lib/"
